604cadd79f67db6d8751a4be3ce6ec6d1a822dfb
connertran <conner.tran@bsa.edu.vn>
2024-01-09 14:28:42 +0100

    adding Jasmine testing to tip tool - try to understand the Springboard solution by changing some values in the tests

--- jasmine-testing-exercises-starter/tip-pool/servers.test.js
+++ jasmine-testing-exercises-starter/tip-pool/servers.test.js
@@ -1,4 +1,4 @@
-describe("Servers test (with setup and tear-down)", function() {
+describe("Servers test (with setup and tear-down)", function () {
   beforeEach(function () {
     // initialization logic
     serverNameInput.value = 'Alice';
@@ -11,7 +11,22 @@ describe("Servers test (with setup and tear-down)", function() {
     expect(allServers['server' + serverId].serverName).toEqual('Alice');
   });
 
-  afterEach(function() {
-    // teardown logic
+  it('should update #servertable on updateServerTable()', function () {
+    submitServerInfo();
+    updateServerTable();
+
+    let curTdList = document.querySelectorAll('#serverTable tbody tr td');
+    // it's three because we have to include the delete button
+    expect(curTdList.length).toEqual(3);
+    expect(curTdList[0].innerText).toEqual('Alice');
+    expect(curTdList[1].innerText).toEqual('$0.00');
+    expect(curTdList[2].innerText).toEqual('X');
   });
+
+  afterEach(function () {
+    serverId = 0;
+    serverTbody.innerHTML = '';
+    allServers = {};
+  });
+
 });
